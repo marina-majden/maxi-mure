9bd4e4dbd1162d6bbf70d2954c9aaeb7c1e09c77
Marina Majdenić <marina.majden@gmail.com>
2025-01-21 10:02:42 +0100

    add cursor animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,44 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <div>
-            <Navbar />
+    /*   const blobCursor = function () {
+        const cursor = document.querySelector("#cursor-blob");
+        const link = document.querySelector("li");
 
+        const setCursorPos = (e) => {
+            let posX = e.pageX;
+            let posY = e.pageY;
+
+            cursor.style.top = posY - cursor.offsetHeight / 2 + "px";
+            cursor.style.left = posX - cursor.offsetWidth / 2 + "px";
+        };
+
+        document.addEventListener("mousemove", setCursorPos);
+
+        const setCursorHover = function () {
+            cursor.style.transform = "scale(2.5)";
+        };
+        const removeCursorHover = function () {
+            cursor.style.transform = "";
+        };
+
+        link.addEventListener("mouseover", setCursorHover);
+        link.addEventListener("mouseleave", removeCursorHover);
+    };
+
+    blobCursor(); */
+    return (
+        <>
+            <div id='cursor-blob'> </div>
+            <Navbar />
             <Home />
             <About />
             <Products />
             <Reviews />
-            <Contact />
 
+            <Contact />
             <Footer />
-        </div>
+        </>
     );
 }
 
